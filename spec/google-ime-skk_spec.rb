08da01ce65e9afad111414a48f987eee82314c33
tomykaira <tomykaira@gmail.com>
2011-05-24 07:42:04 +0900

    Expanded tests

--- spec/google-ime-skk_spec.rb
+++ spec/google-ime-skk_spec.rb
@@ -3,8 +3,29 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'kconv'
 
 describe "GoogleImeSkk" do
+  before :all do
+    @ime = GoogleImeSkk.new("localhost", 12343, nil, 3600)
+  end
+
   it "can search" do
-    ime = GoogleImeSkk.new("localhost", 12343, nil, 3600)
-    Kconv.toutf8(ime.social_ime_search(Kconv.toeuc("ねこ"))).should match "猫"
+    Kconv.toutf8(@ime.social_ime_search(Kconv.toeuc("ねこ"))).should match "猫"
+  end
+end
+
+describe "GoogleImeSkk::CLI" do
+  it "can calculate by day" do
+    GoogleImeSkk::CLI.time_parse("1d").should == 24*60*60
+  end
+  it "can calculate by hour" do
+    GoogleImeSkk::CLI.time_parse("2h").should == 2*60*60
+  end
+  it "can calculate by minute" do
+    GoogleImeSkk::CLI.time_parse("1m").should == 60
+  end
+  it "can calculate by second" do
+    GoogleImeSkk::CLI.time_parse("13s").should == 13
+  end
+  it "can calculate without suffix" do
+    GoogleImeSkk::CLI.time_parse("13").should == 13
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib', 'google-ime-skk'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'google-ime-skk'
+require 'cli'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
